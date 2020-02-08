87aad79a7cf0f856160f903cf8a824164d276d6f
Cartachana <cartachan@gmail.com>
2020-02-08 18:08:48 +0000

    Fresh Start

--- app/src/main/java/com/example/qainfomate/MainActivity.java
+++ app/src/main/java/com/example/qainfomate/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.qainfomate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,12 +9,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText email, pass;
-    private Button login;
+    private Button btnLogin;
     private TextView register;
+    private FirebaseAuth fbAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +31,32 @@ public class MainActivity extends AppCompatActivity {
 
         email = findViewById(R.id.et_email_main);
         pass = findViewById(R.id.et_pass_main);
-        login = findViewById(R.id.btn_Login_main);
+        btnLogin = findViewById(R.id.btn_Login_main);
         register = findViewById(R.id.tv_reg_main);
+
+        fbAuth = FirebaseAuth.getInstance();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fbAuth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+
+                        //to be changed when WELCOME DASHBOARD IS MADE
+                        Toast.makeText(MainActivity.this, "Successfully Logged in", Toast.LENGTH_LONG).show();
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //TO CODE depending on the log in problem for now I will leave a toast of nonexistant user
+                        Toast.makeText(MainActivity.this, "Wrong Credentials.\n Have you registered?", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
