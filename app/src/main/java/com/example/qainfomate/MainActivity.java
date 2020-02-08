83e8c697bfdde747d0da85f2105c4e23a0381d13
Cartachana <cartachan@gmail.com>
2020-02-08 17:47:17 +0000

    Fresh Start

--- app/src/main/java/com/example/qainfomate/MainActivity.java
+++ app/src/main/java/com/example/qainfomate/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.qainfomate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText email, pass;
+    private Button login;
+    private TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.et_email_main);
+        pass = findViewById(R.id.et_pass_main);
+        login = findViewById(R.id.btn_Login_main);
+        register = findViewById(R.id.tv_reg_main);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, Register.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/qainfomate/User.java
+++ app/src/main/java/com/example/qainfomate/User.java
@@ -3,12 +3,10 @@ package com.example.qainfomate;
 public class User {
     private String stuID, fname, sname, email, password;
 
-    public User(String stuID, String fname, String sname, String email, String password) {
+    public User(String stuID, String fname, String sname) {
         this.stuID = stuID;
         this.fname = fname;
         this.sname = sname;
-        this.email = email;
-        this.password = password;
     }
 
     public User() {
@@ -38,19 +36,4 @@ public class User {
         this.sname = sname;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
