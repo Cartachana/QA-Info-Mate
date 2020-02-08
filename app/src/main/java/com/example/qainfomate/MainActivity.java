6869292110a92fe581081122248997e4374fbbf9
Marco <cartachan@gmail.com>
2020-02-08 14:18:50 +0000

    meeting

--- app/src/main/java/com/example/qainfomate/MainActivity.java
+++ app/src/main/java/com/example/qainfomate/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.qainfomate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText em, pass;
+    private Button login, register;
+    //2. DatabaseReference
+    DatabaseReference dbref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
