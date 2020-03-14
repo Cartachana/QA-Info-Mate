d88a604d9ae69383ea70c79a660c586f7100318f
Cartachana <cartachan@gmail.com>
2020-03-14 13:02:22 +0000

    Library Book Details and reviews now live

--- app/src/main/java/com/example/qainfomate/View/Post_review.java
+++ app/src/main/java/com/example/qainfomate/View/Post_review.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -28,6 +31,21 @@ public class Post_review extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_review);
+
+        // This section is the set up and size of the pop up "window"
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.7),(int)(height*.6));
+        WindowManager.LayoutParams params = getWindow().getAttributes();
+        params.gravity = Gravity.CENTER;
+        params.x = 0;
+        params.y = -20;
+        getWindow().setAttributes(params);
+        //Until here
 
         post = findViewById(R.id.btn_post_postRev);
         rev = findViewById(R.id.et_review_postRev);
