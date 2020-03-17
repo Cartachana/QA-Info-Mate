42a9b5d9c0108b4b1e2920ef44558e22d7fc91f9
Cartachana <cartachan@gmail.com>
2020-03-17 19:44:41 +0000

    Library slide in menu done Filter by user experience and back to dashboard working

--- app/src/main/java/com/example/qainfomate/View/Library.java
+++ app/src/main/java/com/example/qainfomate/View/Library.java
@@ -12,6 +12,8 @@ import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import com.example.qainfomate.Adapters.LibraryAdapter;
 import com.example.qainfomate.Models.Library_Book;
 import com.example.qainfomate.R;
@@ -20,6 +22,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
@@ -37,12 +40,26 @@ public class Library extends AppCompatActivity implements LibraryAdapter.Holder.
     private ArrayList<Library_Book> list = new ArrayList<>();
     private ArrayList<String> keys = new ArrayList<>();
     private DatabaseReference dbref;
-    private ImageView closeMenu;
+    private ImageView closeMenu, back;
+    private TextView mine, all, programming, business, economics, web, maths, ux, backDash;
+    private Intent i;
+    private Query dbQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_library);
+
+        back = findViewById(R.id.iv_back_library);
+        mine = findViewById(R.id.tv_mine_library);
+        all = findViewById(R.id.tv_ALL_library);
+        programming = findViewById(R.id.tv_programming_library);
+        business = findViewById(R.id.tv_business_library);
+        economics = findViewById(R.id.tv_economics_library);
+        web = findViewById(R.id.tv_web_library);
+        maths = findViewById(R.id.tv_maths_library);
+        ux = findViewById(R.id.tv_UX_library);
+        backDash = findViewById(R.id.tv_back_library);
 
         RecView = findViewById(R.id.rv_rv_library);
         manager = new GridLayoutManager(this, 3);
@@ -74,6 +91,28 @@ public class Library extends AppCompatActivity implements LibraryAdapter.Holder.
             }
         });
 
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(Library.this, Dashboard.class);
+                startActivity(i);
+            }
+        });
+        backDash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                i = new Intent(Library.this, Dashboard.class);
+                startActivity(i);
+            }
+        });
+
+        ux.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dbQuery = FirebaseDatabase.getInstance().getReference().child("Library_Books").orderByChild("category").equalTo("User Experience");
+                dbQuery.addListenerForSingleValueEvent(listener);
+            }
+        });
     }
 
     ValueEventListener listener = new ValueEventListener() {
