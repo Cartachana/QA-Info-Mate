81a44ad6c19953526f8186cf8a245556c50dc4b4
Cartachana <cartachan@gmail.com>
2020-04-08 11:10:14 +0100

    DONE

--- app/src/main/java/com/example/qainfomate/View/MainActivity.java
+++ app/src/main/java/com/example/qainfomate/View/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,7 +15,9 @@ import android.widget.Toast;
 import com.example.qainfomate.R;
 import com.example.qainfomate.Application.Session;
 import com.example.qainfomate.Models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -26,8 +29,8 @@ import com.google.firebase.database.ValueEventListener;
 public class MainActivity extends AppCompatActivity {
 
     private EditText id, email, pass;
-    private Button btnLogin;
-    private TextView register, please;
+    private Button btnLogin, register;
+    private TextView please, reset;
     private FirebaseAuth fbAuth;
     private Query dbref;
     private Intent i;
@@ -42,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
         email = findViewById(R.id.et_email_main);
         pass = findViewById(R.id.et_pass_main);
         btnLogin = findViewById(R.id.btn_Login_main);
-        register = findViewById(R.id.tv_reg_main);
+        register = findViewById(R.id.btn_reg_main);
+        reset = findViewById(R.id.tv_reset_main);
         please = findViewById(R.id.tv_please_main);
         fbAuth = FirebaseAuth.getInstance();
 
@@ -75,42 +79,39 @@ public class MainActivity extends AppCompatActivity {
 
             else{
                 //IF NO FIELD IS EMPTY THE APP WILL ATTEMPT TO SIGN THE USER IN
-                fbAuth.signInWithEmailAndPassword(email.getText().toString(), pass.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                    @Override
-                    public void onSuccess(AuthResult authResult) {
-                        ValueEventListener listener = new ValueEventListener() {
-                            @Override
-                            //the listener will get every record with the same ID as the ID entered
-                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                                for(DataSnapshot dss:dataSnapshot.getChildren())
-                                {//but will only get the one with the same primary key as the UID in the Authentication
-                                    if(fbAuth.getCurrentUser().getUid().matches(dss.getKey())) {
-                                        Session.LiveSession.user = dss.getValue(User.class);
-                                        Session.LiveSession.setContext(appContext);
-                                    }
-                                }
-                                if(Session.LiveSession.user != null)
-                                {
-                                    Toast.makeText(MainActivity.this, Session.LiveSession.user.getFname() + " " +
-                                            "" + Session.LiveSession.user.getSname() + " " + "Logged in", Toast.LENGTH_LONG).show();
-                                    i = new Intent(MainActivity.this, Dashboard.class);
-                                    startActivity(i);
-                                    //if the ID does not match the email
-                                } else{
-                                    please.setText("Wrong Student ID entered");
-                                    please.setVisibility(View.VISIBLE);
-                                    fbAuth.signOut();
+                //if sign in fails, text is shown to user saying credentials were not recognized and asking if he registered
+                fbAuth.signInWithEmailAndPassword(email.getText().toString().trim(), pass.getText().toString().trim()).addOnSuccessListener(authResult -> {
+                    ValueEventListener listener = new ValueEventListener() {
+                        @Override
+                        //the listener will get every record with the same ID as the ID entered
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            for(DataSnapshot dss:dataSnapshot.getChildren())
+                            {//but will only get the one with the same primary key as the UID in the Authentication
+                                if(fbAuth.getCurrentUser().getUid().matches(dss.getKey())) {
+                                    Session.LiveSession.user = dss.getValue(User.class);
+                                    Session.LiveSession.setContext(appContext);
                                 }
                             }
-                            @Override
-                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                            if(Session.LiveSession.user != null)
+                            {
+                                Toast.makeText(MainActivity.this, Session.LiveSession.user.getFname() + " " +
+                                        "" + Session.LiveSession.user.getSname() + " " + "Logged in", Toast.LENGTH_LONG).show();
+                                i = new Intent(MainActivity.this, Dashboard.class);
+                                startActivity(i);
+                                //if the ID does not match the email
+                            } else{
+                                please.setText("Wrong Student ID entered");
+                                please.setVisibility(View.VISIBLE);
+                                fbAuth.signOut();
                             }
-                        };
-                        //our database query will get all users stuID in the databse that are equal to the ID entered
-                        dbref = FirebaseDatabase.getInstance().getReference("_user_").orderByChild("stuID").equalTo(id.getText().toString());
-                        dbref.addListenerForSingleValueEvent(listener);
-                    }
-                    //if sign in fails, text is shown to user saying credentials were not recognized and asking if he registered
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+                        }
+                    };
+                    //our database query will get all users stuID in the databse that are equal to the ID entered
+                    dbref = FirebaseDatabase.getInstance().getReference("_user_").orderByChild("stuID").equalTo(id.getText().toString().trim());
+                    dbref.addListenerForSingleValueEvent(listener);
                 }).addOnFailureListener(e -> {
                     Toast.makeText(MainActivity.this, "Wrong Credentials.\nHave you registered?", Toast.LENGTH_LONG).show();
                     please.setText("Wrong Student ID entered");
@@ -118,6 +119,25 @@ public class MainActivity extends AppCompatActivity {
                 });
             }});
 
+        reset.setOnClickListener(v -> {
+
+            String enteredEm = email.getText().toString().trim();
+
+            if (TextUtils.isEmpty(enteredEm)) {
+                please.setText("Enter your registered email address");
+                please.setVisibility(View.VISIBLE);
+                return;
+            }
+
+            fbAuth.sendPasswordResetEmail(enteredEm)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Password Reset Email Sent!", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Failed to send reset email!", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
 
         register.setOnClickListener(v -> {
             i = new Intent(MainActivity.this, Register.class);
